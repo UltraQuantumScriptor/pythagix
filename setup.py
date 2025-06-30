15d02a0150645d05edc6d23f56d1492ea72a79f7
Ultra QuantumScriptor <quantumscriptor@gmail.com>
2025-06-30 19:12:57 +0500

    organized all functions and added variance function

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", encoding="utf-8") as f:
 
 setup(
     name="pythagix",
-    version="0.2.7",
+    version="0.2.8",
     author="UltraQuantumScriptor",
     description="A mathy Python package with utilities like LCM, triangle numbers, etc.",
     long_description=long_description,
